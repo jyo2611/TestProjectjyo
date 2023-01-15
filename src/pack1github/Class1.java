81cfa0ec10faf4f69ef3db795240c1453af44bea
dell <dell@DESKTOP-JM01L4U>
2023-01-15 13:40:34 +0530

    testr-jyoti date-15 jan 13.40pm

--- src/pack1github/Class1.java
+++ src/pack1github/Class1.java
@@ -5,6 +5,8 @@ public class Class1
 
 	public static void main(String[] args) 
 	   {
-	        System.out.println("This is sample other git project");	
+	        System.out.println("This is sample other git project");
+	        System.out.println("This is git project");
+       
 	}
 }
